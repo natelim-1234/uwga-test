2df6e5c429296d9571ecc52710e63a2cc5f16cb5
natelim-1234 <72533102+natelim-1234@users.noreply.github.com>
2026-04-28 06:51:55 +0000

    Deploying to gh-pages from @ natelim-1234/uwga-test@d353112d98936c7695d18247b3cc6abecb233d9e 🚀

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// For GitHub Pages: set base to your repo name, e.g. '/my-repo/'
-// For root deployment (custom domain): keep './'
-export default defineConfig({
-  plugins: [react()],
-  base: '/uwga-test/',
-})
